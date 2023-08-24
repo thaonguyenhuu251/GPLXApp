425e157d2347afa8e55ef58e03dce3ff6f9e5ba8
NguyenHuuThao251 <nguyenhuuthao2001@gmail.com>
2023-08-24 11:32:19 +0700

    FIX ERROR

--- app/src/main/java/com/htnguyen/gplxapp/MainApp.kt
+++ app/src/main/java/com/htnguyen/gplxapp/MainApp.kt
@@ -7,22 +7,22 @@ import com.htnguyen.gplxapp.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MainApp : Application() {
     companion object {
         lateinit var instance: MainApp
-            private set
     }
 
     private var currentActivity: Activity? = null
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
+            androidLogger(Level.NONE)
             androidContext(this@MainApp)
             modules(listOf(
                 viewModelModule,
-                adapterModule,
+                adapterModule
             ))
 
         }
